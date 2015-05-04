4d5a849402498e364d613deb0801a7bf236599b4
Todd Kennedy <todd@selfassembled.org>
2015-05-04 16:13:57 -0400

    fixing tests

--- test/test.js
+++ test/test.js
@@ -22,10 +22,10 @@ test('no rel', function(t) {
 });
 
 test('rel', function(t) {
-  var results = ["exports.testFile1 = require('/Users/todd/src/commonjs-concat/test/fixtures/test-file-1.js');",
-    "exports.testFile2 = require('/Users/todd/src/commonjs-concat/test/fixtures/test_file-2.js');",
-    "exports.testing = require('/Users/todd/src/commonjs-concat/test/fixtures/testing.json');",
-    "exports.testing1 = require('/Users/todd/src/commonjs-concat/test/fixtures/more/testing.js');",
+  var results = ["exports.testFile1 = require('"+__dirname+"/fixtures/test-file-1.js');",
+    "exports.testFile2 = require('"+__dirname+"/fixtures/test_file-2.js');",
+    "exports.testing = require('"+__dirname+"/fixtures/testing.json');",
+    "exports.testing1 = require('"+__dirname+"/fixtures/more/testing.js');",
   ];
 
   concat('./fixtures', {
@@ -40,13 +40,12 @@ test('rel', function(t) {
 });
 
 test('no json', function(t) {
-  var results = ["exports.testFile1 = require('/Users/todd/src/commonjs-concat/test/fixtures/test-file-1.js');",
-    "exports.testFile2 = require('/Users/todd/src/commonjs-concat/test/fixtures/test_file-2.js');",
-    "exports.testing = require('/Users/todd/src/commonjs-concat/test/fixtures/more/testing.js');",
+  var results = ["exports.testFile1 = require('./test-file-1.js');",
+    "exports.testFile2 = require('./test_file-2.js');",
+    "exports.testing = require('./more/testing.js');",
   ];
 
   concat('./fixtures', {
-    relative: true,
   }, function(err, concated) {
     concated.split('\n').forEach(function(file){
       t.notEqual(results.indexOf(file), -1, file+' located');
